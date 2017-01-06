426d2ae4dbe4a5d66e26ab67a9111809dce0fefc
Shion <a951753abc@gmail.com>
2017-01-07 03:49:13 +0800

    blade

---
+++ resources/views/events/event.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.layout')
+
+@section('content')
+    <p>這是我的主要內容。</p>
+@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>LARP TOOL</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}"></script>
+
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @if (Auth::check())
+                <a href="{{ url('/home') }}">Home</a>
+            @else
+                <a href="{{ url('/login') }}">Login</a>
+                <a href="{{ url('/register') }}">Register</a>
+            @endif
+        </div>
+    @endif
+
+    <div class="content">
+        @yield('content')
+    </div>
+</div>
+</body>
+</html>
