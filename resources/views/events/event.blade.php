1e27fb56938c65d245f110480af6cf97a62e5c80
Shion <a951753abc@gmail.com>
2017-01-07 22:04:02 +0800

    change size

--- resources/views/events/event.blade.php
+++ resources/views/events/event.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="row">
-        <div class="card" style="width: 25rem;">
+        <div class="card" style="width: 20rem;">
             <img class="card-img-top" src="https://c2.staticflickr.com/2/1605/25228346924_2e927d2a50.jpg" alt="Card image cap">
             <div class="card-block">
                 <h4 class="card-title">{{$event['name']}}</h4>
